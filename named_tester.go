7ca234f9a85955aa5bf190ae6e231be7f91ba4cb
amidgo <mcdvaran@gmail.com>
2024-09-07 12:03:32 +0400

    remove container

--- named_tester.go
+++ named_tester.go
@@ -20,37 +20,15 @@ func (nt *NamedTesterFunc) Test(t *testing.T) {
 	nt.f(t)
 }
 
-func NewNamedTester(name string, f func(t *testing.T)) NamedTester {
+func NamedTester(name string, f func(t *testing.T)) NamedTester {
 	return &NamedTesterFunc{
 		name: name,
 		f:    f,
 	}
 }
 
-type NamedTesterContainer struct {
-	namedTesters []NamedTester
-}
-
 func RunNamedTesters(t *testing.T, namedTesters ...NamedTester) {
 	for _, tester := range namedTesters {
-		if tester == nil {
-			continue
-		}
-
-		t.Run(tester.Name(), tester.Test)
-	}
-}
-
-func (c *NamedTesterContainer) AddNamedTester(tester NamedTester) {
-	c.namedTesters = append(c.namedTesters, tester)
-}
-
-func (c *NamedTesterContainer) AddNamedTesters(testers ...NamedTester) {
-	c.namedTesters = append(c.namedTesters, testers...)
-}
-
-func (c *NamedTesterContainer) Test(t *testing.T) {
-	for _, tester := range c.namedTesters {
 		if tester == nil {
 			continue
 		}
--- test/named_tester_test.go
+++ test/named_tester_test.go
@@ -7,38 +7,6 @@ import (
 	"github.com/amidgo/tester/mock"
 )
 
-func Test_NamedTesterContainer(t *testing.T) {
-	var tester tester.NamedTesterContainer
-
-	tester.AddNamedTester(mock.NewMockedNamedTester(t))
-	tester.AddNamedTester(mock.NewMockedNamedTester(t))
-	tester.AddNamedTester(mock.NewMockedNamedTester(t))
-	tester.AddNamedTester(mock.NewMockedNamedTester(t))
-
-	tester.AddNamedTesters(
-		mock.NewMockedNamedTester(t),
-		mock.NewMockedNamedTester(t),
-		mock.NewMockedNamedTester(t),
-		mock.NewMockedNamedTester(t),
-	)
-
-	tester.Test(t)
-}
-
-func Test_NamedTesterContainer_Not_Test_Call_Case(t *testing.T) {
-	var tester tester.NamedTesterContainer
-
-	nt := new(mock.MockedNamedTester)
-
-	tester.AddNamedTester(nt)
-
-	tester.AddNamedTesters(
-		nt,
-	)
-
-	nt.AssertNotCalled(t)
-}
-
 func Test_RunNamedTesters(t *testing.T) {
 	tester.RunNamedTesters(
 		t,
